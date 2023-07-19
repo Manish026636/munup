9b6572bb17148d2e4b0e303331c477b84b49e723
Manish <my79220@gmail.com>
2023-07-19 20:21:53 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Common/Navbar';
 import Dashboard from './components/Chairperson/Dashboard';
+import MCDashboard from './components/MCAUS/MCDashboard';
 function App() {
   return (
     <div className="flex">
     <Navbar />
     <div className="flex-grow">
-      <Dashboard/>
+      <MCDashboard/>
     </div>
   </div>
   );
